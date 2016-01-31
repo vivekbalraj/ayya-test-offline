d0a3502d66e9acc9f7f0a243247d0bdcd23bc4fa
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-01-31 13:39:24 +0530

    testimonials active admin

--- app/admin/testimonial.rb
+++ app/admin/testimonial.rb
@@ -1,18 +1,25 @@
 ActiveAdmin.register Testimonial do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
   permit_params :name, :village, :text
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
+  config.clear_sidebar_sections!
+
+  index do
+    id_column
+    column :name
+    column :village
+    column :text
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs "Label" do
+      f.input :name
+      f.input :village
+      f.input :text
+    end
+    f.actions
+  end
 
 end
