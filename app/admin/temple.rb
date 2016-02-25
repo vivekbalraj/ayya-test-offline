115204bac4662de57e96be11ee792b0e288245f1
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-02-25 12:59:49 +0530

    style fixes for active admin

--- app/admin/temple.rb
+++ app/admin/temple.rb
@@ -2,7 +2,9 @@ ActiveAdmin.register Temple do
 
   permit_params :name, :temple_type, :information, :village, :latitude, :longitude, :district
 
-  config.clear_sidebar_sections!
+  filter :district, :as => :select, :collection => ["அரியலூர்", "சென்னை", "கோயம்புத்தூர்", "கடலூர்", "தர்மபுரி", "திண்டுக்கல்", "ஈரோடு", "காஞ்சிபுரம்", "கன்னியாகுமரி", "கரூர்", "கிருஷ்ணகிரி", "மதுரை", "நாகப்பட்டினம்", "நாமக்கல்", "பெரம்பலூர்", "புதுக்கோட்டை", "இராமநாதபுரம்", "சேலம்", "சிவகங்கை", "தஞ்சாவூர்", "தேனி", "நீலகிரி", "திருநெல்வேலி", "திருவள்ளூர்", "திருவண்ணாமலை", "திருவாரூர்", "தூத்துக்குடி", "திருச்சிராப்பள்ளி", "திருப்பூர்", "வேலூர்", "விழுப்புரம்", "விருதுநகர்"]
+
+  filter :temple_type, :as => :select, :collection => ["Pathi", "Thangal"]
 
   index do
     id_column
