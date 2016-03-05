0aaa5d93b3394d0400c5b59947fcca6990261af9
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-03-05 16:57:04 +0530

    device active admin changes

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -2,6 +2,9 @@ ActiveAdmin.register Device do
 
   permit_params :token, :enabled, :platform
 
+  filter :enabled, :as => :select
+  filter :platform, :as => :select
+
   # index do
   #   id_column
   #   column :token
