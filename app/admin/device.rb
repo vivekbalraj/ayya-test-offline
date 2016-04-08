e2d5175bd7937d32a810d93edb36c20a12ed99f3
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-04-08 21:14:24 +0530

    fix device active admin page

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -1,16 +1,5 @@
 ActiveAdmin.register Device do
 
-  permit_params :token, :enabled, :platform
-
-  filter :enabled, :as => :select
-  filter :platform, :as => :select
-
-  # index do
-  #   id_column
-  #   column :token
-  #   column :enabled
-  #   column :platform
-  #   actions
-  # end
+  config.clear_sidebar_sections!
 
 end
