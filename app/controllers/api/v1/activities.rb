1f98c56144adeff5718013fc20ef1f1ab99a117f
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-04-12 22:18:49 +0530

    removing duplicates

--- app/controllers/api/v1/activities.rb
+++ app/controllers/api/v1/activities.rb
@@ -12,15 +12,17 @@ module API
           response = Hash.new { "response" }
           activities.each do |activity|
             if activity['trackable_type'] == 'Temple'
-              temple = Temple.select('id, name, information, temple_type, village, district, latitude, longitude, founded_at, contact_person, country, state, taluk, pincode, street_address, is_primary_thangal, is_book_read, book_month, is_published, priest_name, facebook_page_url, img1_file_name, img2_file_name, img3_file_name').includes(:events, :cars).find(activity['trackable_id']).as_json(include: [:events, :cars], :methods => [:images, :thumb])
-              temples.push temple
+              # temple = Temple.select('id, name, information, temple_type, village, district, latitude, longitude, founded_at, contact_person, country, state, taluk, pincode, street_address, is_primary_thangal, is_book_read, book_month, is_published, priest_name, facebook_page_url, img1_file_name, img2_file_name, img3_file_name').includes(:events, :cars).find(activity['trackable_id']).as_json(include: [:events, :cars], :methods => [:images, :thumb])
+              temples.push activity['trackable_id']
             elsif activity['trackable_type'] == 'Notification'
               notification = Notification.select(:message, :id, :title).find(activity['trackable_id']).as_json(:methods => [:picture_url])
               notifications.push notification
             end
           end
+          temples = temples.uniq
+          notifications = notifications.uniq
           response[:activities] = activities
-          response[:temples] = temples
+          response[:temples] = Temple.select('id, name, information, temple_type, village, district, latitude, longitude, founded_at, contact_person, country, state, taluk, pincode, street_address, is_primary_thangal, is_book_read, book_month, is_published, priest_name, facebook_page_url, img1_file_name, img2_file_name, img3_file_name').includes(:events, :cars).where(id: temples).as_json(include: [:events, :cars], :methods => [:images, :thumb])
           response[:notifications] = notifications
           response
         end
