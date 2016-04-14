a1d98fd85512541b5366d04200ca5cd71150b9d3
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-04-14 15:09:38 +0530

    minor fix

--- app/controllers/api/v1/activities.rb
+++ app/controllers/api/v1/activities.rb
@@ -21,7 +21,7 @@ module API
           temples = temples.uniq
           notifications = notifications.uniq
           response[:activities] = activities
-          response[:temples] = Temple.select('id, name, information, temple_type, village, district, latitude, longitude, founded_at, contact_person, country, state, taluk, pincode, street_address, is_primary_thangal, is_book_read, book_month, is_published, priest_name, facebook_page_url, img1_file_name, img2_file_name, img3_file_name').includes(:events, :cars).where(id: temples).as_json(include: [:events, :cars], :methods => [:images, :thumb])
+          response[:temples] = Temple.select('id, name, information, temple_type, village, district, latitude, longitude, founded_at, country, state, taluk, pincode, street_address, is_primary_thangal, is_book_read, book_month, is_published, priest_name, facebook_page_url, img1_file_name, img2_file_name, img3_file_name').includes(:events, :cars).where(id: temples).as_json(include: [:events, :cars], :methods => [:images, :thumb])
           response[:notifications] = notifications
           response
         end
