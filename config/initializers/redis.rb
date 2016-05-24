e998359e296b3dd11d46989afbe4b33f2d8ad6b0
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-05-24 09:36:28 +0530

    redis url change again

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: 'redis://h:p3bp1aip8l275ll7sgejn6nuhv@ec2-107-22-214-1.compute-1.amazonaws.com:16549')
+$redis = Redis.new(url: 'redis://h:p3bp1aip8l275ll7sgejn6nuhv@ec2-54-225-224-106.compute-1.amazonaws.com:7099')
