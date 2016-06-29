0ce07360a65086210d38f3d4bab4fa1a4b4d81f2
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-06-29 23:00:49 +0530

    few updates

--- app/controllers/api/v1/temples.rb
+++ app/controllers/api/v1/temples.rb
@@ -52,6 +52,8 @@ module API
         desc "create a temple"
         post do
           temple = Temple.create(name: params[:name], founded_at: params[:founded_at], book_month: params[:book_month], contact_person: params[:contact_person], mobile_number: params[:mobile_number], village: params[:village], taluk: params[:taluk], district: params[:district], latitude: params[:latitude], longitude: params[:longitude], pincode: params[:pincode], street_address: params[:street_address], information: params[:information], facebook_page_url: params[:facebook_page_url], priest_name: params[:priest_name])
+          temple.cars = Car.find(params[:cars])
+          temple.save
           TempleNotifier.send_temple_email(temple).deliver
         end
       end
