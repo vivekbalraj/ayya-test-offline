15177c303de051949372f8157719b18300515d28
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-08-13 20:45:26 +0530

    FE build

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811170643) do
+ActiveRecord::Schema.define(version: 20160811171338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,6 +175,8 @@ ActiveRecord::Schema.define(version: 20160811170643) do
     t.string   "mobile"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
