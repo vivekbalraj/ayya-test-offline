071aa0246a567ec8275db315109f9484fc2afbce
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-09-03 21:55:51 +0530

    fe map center

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -17,6 +17,10 @@ export class MainController {
       vm.imageTemples = _.chain(vm.temples).filter((temple) => temple.thumb !== '/img1s/thumb/missing.png' && temple.information).shuffle().value();
       vm.imageTemples = _.sortBy(vm.imageTemples, 'temple_type');
       vm.mapTemples = vm.temples.filter(temple => !!temple.latitude);
+      vm.mapCenter = {
+        latitude: (parseFloat(_.maxBy(vm.mapTemples, (temple) => parseFloat(temple.latitude)).latitude) + parseFloat(_.minBy(vm.mapTemples, (temple) => parseFloat(temple.latitude)).latitude)) / 2,
+        longitude: (parseFloat(_.maxBy(vm.mapTemples, (temple) => parseFloat(temple.longitude)).longitude) + parseFloat(_.minBy(vm.mapTemples, (temple) => parseFloat(temple.longitude)).longitude)) / 2
+      };
       vm.isTemplesLoaded = true;
     });
   }
