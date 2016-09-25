62bc0310dede42d16ea2ffed2063be8af08f30fa
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2016-09-25 20:32:07 +0530

    active admin for users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,6 @@
 ActiveAdmin.register User do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  permit_params :name, :email, :mobile, :gender, :birthday, :facebook_id
 
   index do
     selectable_column
@@ -21,6 +9,8 @@ ActiveAdmin.register User do
     column :email
     column :mobile
     column :gender
+    column :birthday
+    column :facebook_id
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
